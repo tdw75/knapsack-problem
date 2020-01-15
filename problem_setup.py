c9641d7f6470b0a84a44ee2d08a4e664a8e7f57c
tdw75 <tdw75@outlook.com>
2020-01-15 23:34:06 +0100

    Added value density of items

--- problem_setup.py
+++ problem_setup.py
@@ -1,9 +1,8 @@
 from collections import namedtuple
-import numpy as np
 
 
 def parse_input_data(input_data):
-    Item = namedtuple("Item", ['index', 'value', 'weight'])
+    Item = namedtuple("Item", ['index', 'value', 'weight', 'density'])
     lines = input_data.split('\n')
 
     first_line = lines[0].split()
@@ -15,14 +14,19 @@ def parse_input_data(input_data):
     for i in range(1, item_count + 1):
         line = lines[i]
         parts = line.split()
-        items.append(Item(i - 1, int(parts[0]), int(parts[1])))
+        items.append(Item(i - 1, int(parts[0]), int(parts[1]), int(parts[0]) / int(parts[1])))
 
     return items, capacity
 
 
-def output_solution(decision_variables, obj_value):
+def output_solution(decision_variables, obj_value, optimal=False):
 
-    output_data = str(obj_value) + ' ' + str(1) + '\n'
+    if optimal is True:
+        opt = 1
+    else:
+        opt = 0
+
+    output_data = str(obj_value) + ' ' + str(opt) + '\n'
     output_data += ' '.join(map(str, decision_variables))
 
     return output_data
