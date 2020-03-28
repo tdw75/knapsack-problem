f3aaacc97a3026c6d82fdedfe4aa8e33e7aaa5d9
tdw75 <tdw75@outlook.com>
2020-03-28 11:27:18 +0100

    test depth first branch and bound solver

--- test/branch_and_bound_test.py
+++ test/branch_and_bound_test.py
@@ -6,30 +6,69 @@ from branch_and_bound import BranchAndBound
 class TestBranchAndBound(TestCase):
 
     def setUp(self) -> None:
-        with open('resources\data\ks_test', 'r') as file:
+        with open('resources\data\ks_4_0', 'r') as file:
             self.input_data = file.read()
 
-    def test_that_relaxed_solution_with_fixed_variables_is_correctly_calculated(self):
+    def test_that_relaxed_solution_with_no_variables_fixed_is_correctly_calculated(self):
         bnb = BranchAndBound(self.input_data)
-        fixed_vars = {1: 1}
-        solution = bnb.relaxed_solution(fixed_vars=fixed_vars)
-        expected_solution = [0, 1, 1]
+        fixed_vars = [None, None, None, None]
+        solution = bnb.relaxed_solution(fixed_variables=fixed_vars)
+        expected_solution = [0, 1, 1, 2 / 8]
         self.assertListEqual(expected_solution, solution)
 
-    def test_that_relaxed_solution_without_fixed_variables_is_correctly_calculated(self):
+    def test_that_relaxed_solution_with_some_variables_fixed_is_correctly_calculated(self):
         bnb = BranchAndBound(self.input_data)
-        solution = bnb.relaxed_solution(fixed_vars=None)
-        expected_solution = [1, 0.5, 0]
+        fixed_vars = [None, 1, 0, None]
+        solution = bnb.relaxed_solution(fixed_variables=fixed_vars)
+        expected_solution = [0, 1, 0, 7 / 8]
         self.assertListEqual(expected_solution, solution)
+
+    def test_that_relaxed_solution_with_all_variables_fixed_is_correctly_calculated(self):
+        bnb = BranchAndBound(self.input_data)
+        fixed_vars = [1, 1, 0, 0]
+        solution = bnb.relaxed_solution(fixed_variables=fixed_vars)
+        self.assertListEqual(fixed_vars, solution)
+
+    def test_that_infeasible_solution_returns_empty_list(self):
+        bnb = BranchAndBound(self.input_data)
+        fixed_vars = [1, 1, 0, 1]
+        solution = bnb.relaxed_solution(fixed_variables=fixed_vars)
+        self.assertListEqual([], solution)
 
     def test_that_feasible_solution_is_returned(self):
         bnb = BranchAndBound(self.input_data)
-        solution_feasible = [0, 1, 1]
-        result_feasible = bnb.feasible(solution_feasible)
-        self.assertEqual(result_feasible, True)
+        feasible_solution = [1, 0, 0, 1]
+        result = bnb.feasible(feasible_solution)
+        self.assertEqual(result, True)
 
     def test_that_infeasible_solution_is_identified(self):
         bnb = BranchAndBound(self.input_data)
-        solution_infeasible = [1, 0, 1]
-        result_infeasible = bnb.feasible(solution_infeasible)
-        self.assertEqual(result_infeasible, False)
+        infeasible_solution = [1, 1, 1, 0]
+        result = bnb.feasible(infeasible_solution)
+        self.assertEqual(result, False)
+
+    def test_that_invalid_solution_is_identified(self):
+        bnb = BranchAndBound(self.input_data)
+        invalid_solution = [1, 0.25, 1, 0]
+        result = bnb.valid(invalid_solution)
+        self.assertEqual(result, False)
+
+    def test_that_valid_solution_is_identified(self):
+        bnb = BranchAndBound(self.input_data)
+        valid_solution = [1, 1, 0, 0]
+        result = bnb.valid(valid_solution)
+        self.assertEqual(result, True)
+
+    def test_that_objective_function_value_is_correctly_calculated(self):
+        bnb = BranchAndBound(self.input_data)
+        solution = [1, 0, 0, 1]
+        value = bnb.calculate_objective_value(solution)
+        expected = 19
+        self.assertEqual(expected, value)
+
+    def test_that_optimal_solution_is_found(self):
+        bnb = BranchAndBound(self.input_data)
+        solution, obj = bnb.solve()
+        expected_solution = [1, 0, 0, 1]
+        self.assertListEqual(expected_solution, solution)
+        self.assertEqual(19, obj)
