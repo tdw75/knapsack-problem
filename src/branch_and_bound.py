bdd79617b5a792ee082054bb70eec0f474cb5e93
tdw75 <tdw75@outlook.com>
2020-03-29 00:08:16 +0100

    implement best first branch and bound solver

--- src/branch_and_bound.py
+++ src/branch_and_bound.py
@@ -1,14 +1,19 @@
 import time
 from typing import List, Tuple
 
+import numpy as np
+
 from src.problem_setup import parse_input_data
 
 
 class BranchAndBound:
-    def __init__(self, input_data):
+    def __init__(self, input_data, search_strategy='best_first'):
         self.items, self.capacity = parse_input_data(input_data)
         self.n = len(self.items)
+        self.strategy = search_strategy
         self.sorted_items = sorted(self.items, key=lambda x: x.density, reverse=True)
+        self.ranks = [item.index for item in self.sorted_items]
+        self.ranks = np.argsort(self.ranks)
         self.decision_variables = None
         self.obj_value = None
 
@@ -16,7 +21,7 @@ class BranchAndBound:
 
         decision_variables = fixed_variables.copy()
         items_stack = [self.sorted_items[idx] for idx in range(self.n) if
-                       decision_variables[self.sorted_items[idx].index] is None]
+                       decision_variables[idx] is None]
         remaining_capacity = self.capacity - self.calculate_knapsack_weight(decision_variables)
 
         if remaining_capacity < 0:
@@ -25,12 +30,11 @@ class BranchAndBound:
             return decision_variables
 
         for item in items_stack:
-            # if not decision_variables[item.index]:
             if item.weight <= remaining_capacity:
-                decision_variables[item.index] = 1
+                decision_variables[self.ranks[item.index]] = 1
                 remaining_capacity -= item.weight
             else:
-                decision_variables[item.index] = remaining_capacity / item.weight
+                decision_variables[self.ranks[item.index]] = remaining_capacity / item.weight
                 break
 
         decision_variables = [x if x else 0 for x in decision_variables]
@@ -41,6 +45,11 @@ class BranchAndBound:
     def branch(index: int) -> List[Tuple[int, int]]:
         options = [(index, 0), (index, 1)]
         return options
+
+    def bound(self, stack: List[tuple], current_config: List[int]) -> Tuple[int, List[int]]:
+        current_idx = stack[-1][0]
+        current_config = current_config[:current_idx] + [None] * (self.n - current_idx)
+        return current_idx, current_config
 
     def feasible(self, decision_variables: List[int]) -> bool:
         weight = self.calculate_knapsack_weight(decision_variables)
@@ -57,10 +66,12 @@ class BranchAndBound:
         return True
 
     def calculate_objective_value(self, decision_variables: List[int]) -> bool:
-        return sum([self.items[idx].value for idx in range(self.n) if decision_variables[idx] == 1])
+        return sum([self.sorted_items[idx].value * decision_variables[idx] for idx in range(self.n) if
+                    decision_variables[idx] is not None])
 
     def calculate_knapsack_weight(self, decision_variables):
-        return sum([self.items[idx].weight for idx in range(self.n) if decision_variables[idx] == 1])
+        return sum([self.sorted_items[idx].weight * decision_variables[idx] for idx in range(self.n) if
+                    decision_variables[idx] is not None])
 
     def best_so_far(self, current_best: int, decision_variables: List[int]) -> bool:
         value = self.calculate_objective_value(decision_variables)
@@ -69,7 +80,7 @@ class BranchAndBound:
         else:
             return False
 
-    def solve(self):
+    def solve(self) -> Tuple[List[int], int]:
         best_obj = 0
         best_config = [0] * self.n
         current_idx = 0
@@ -79,30 +90,29 @@ class BranchAndBound:
         next_up = stack.pop(-1)
 
         while stack:
+
             current_config[next_up[0]] = next_up[1]
             candidate_solution = self.relaxed_solution(current_config)
 
             if not candidate_solution:  # infeasible
                 next_up = stack.pop(-1)
 
-            elif self.feasible(candidate_solution) and not self.valid(candidate_solution):
+            elif self.best_so_far(best_obj, candidate_solution) and not self.valid(candidate_solution):
                 current_idx += 1
                 if current_idx >= self.n:
-                    current_idx = stack[-1][0]
+                    current_idx, current_config = self.bound(stack, current_config)
                     continue
                 stack += self.branch(current_idx)
                 next_up = stack.pop(-1)
 
-            elif self.valid(candidate_solution):
-                current_idx = stack[-1][0]
-                current_config = current_config[:current_idx] + [None] * (self.n - current_idx)
+            else:
+                current_idx, current_config = self.bound(stack, current_config)
                 next_up = stack.pop(-1)
                 if self.best_so_far(best_obj, candidate_solution):
                     best_config = candidate_solution
                     best_obj = self.calculate_objective_value(best_config)
 
-        # item_indices = [item.index for item in self.items if best_config[item.index] == 1]
-        item_indices = [item.index for item in self.items if best_config[item.index] == 1]
+        item_indices = [item.index for item in self.sorted_items if best_config[self.ranks[item.index]] == 1]
         solution = [0] * self.n
         for idx in item_indices:
             solution[idx] = 1
@@ -119,4 +129,4 @@ if __name__ == "__main__":
     configuration, obj = bnb.solve()
 
     print("best solution found was {}".format(obj))
-    print("execution time = {:.1f} seconds".format(time.time() - start))
+    print("execution time = {:.2f} seconds".format(time.time() - start))
--- test/branch_and_bound_test.py
+++ test/branch_and_bound_test.py
@@ -6,23 +6,23 @@ from branch_and_bound import BranchAndBound
 class TestBranchAndBound(TestCase):
 
     def setUp(self) -> None:
-        with open('resources\data\ks_4_0', 'r') as file:
+        with open('resources\data\ks_4_0_test', 'r') as file:
             self.input_data_4 = file.read()
-        with open('resources\data\ks_30_0', 'r') as file:
+        with open('resources\data\ks_30_0_test', 'r') as file:
             self.input_data_30 = file.read()
 
     def test_that_relaxed_solution_with_no_variables_fixed_is_correctly_calculated(self):
         bnb = BranchAndBound(self.input_data_4)
         fixed_vars = [None, None, None, None]
         solution = bnb.relaxed_solution(fixed_variables=fixed_vars)
-        expected_solution = [0, 1, 1, 2 / 8]
+        expected_solution = [1, 1, 2 / 8, 0]
         self.assertListEqual(expected_solution, solution)
 
     def test_that_relaxed_solution_with_some_variables_fixed_is_correctly_calculated(self):
         bnb = BranchAndBound(self.input_data_4)
-        fixed_vars = [None, 1, 0, None]
+        fixed_vars = [1, 0, None, None]
         solution = bnb.relaxed_solution(fixed_variables=fixed_vars)
-        expected_solution = [0, 1, 0, 7 / 8]
+        expected_solution = [1, 0, 7 / 8, 0]
         self.assertListEqual(expected_solution, solution)
 
     def test_that_relaxed_solution_with_all_variables_fixed_is_correctly_calculated(self):
@@ -63,7 +63,7 @@ class TestBranchAndBound(TestCase):
 
     def test_that_objective_function_value_is_correctly_calculated(self):
         bnb = BranchAndBound(self.input_data_4)
-        solution = [1, 0, 0, 1]
+        solution = [0, 0, 1, 1]
         value = bnb.calculate_objective_value(solution)
         expected = 19
         self.assertEqual(expected, value)
@@ -72,14 +72,14 @@ class TestBranchAndBound(TestCase):
         bnb = BranchAndBound(self.input_data_4)
         knapsack = [0, 1, 1, None]
         weight = bnb.calculate_knapsack_weight(knapsack)
-        self.assertEqual(weight, 9)
+        self.assertEqual(13, weight)
 
     def test_that_optimal_solution_is_found(self):
         bnb = BranchAndBound(self.input_data_4)
         solution, obj = bnb.solve()
         expected_solution = [1, 0, 0, 1]
-        self.assertListEqual(expected_solution, solution)
         self.assertEqual(19, obj)
+        self.assertListEqual(expected_solution, solution)
 
     def test_that_optimal_solution_is_found_large(self):
         bnb = BranchAndBound(self.input_data_30)
